47e81ea45c9a5ff55c7c73436735619e53f51f24
haydengaoCN <1379658566@qq.com>
2021-11-10 10:34:41 +0800

    multimap

--- multimap/test1.cc
+++ multimap/test1.cc
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <utility>
 #include <string>
+#include <unordered_map>
 #include <map>
  
 int main() {
     std::multimap<std::string, std::string> mm;
+    std::unordered_map<std::string, std::string> um;
     std::map<std::string, std::string> m;
  
     mm.emplace("a", "a1");
     mm.emplace("a", "a2");
     m.emplace("a", "a1");
     m.emplace("a", "a2");
+    um.emplace("a", "a1");
+    um.emplace("a", "a2");
 
  
-    bool flag = false;
-    const auto& t = flag ? mm : m;
-    for (const auto &p : t) {
+    for (const auto &p : um) {
         std::cout << p.first << " => " << p.second << '\n';
     }
 
