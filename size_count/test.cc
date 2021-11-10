c1239143e54e74dee26a9092f991385c0054a353
haydengaoCN <1379658566@qq.com>
2021-11-10 10:34:08 +0800

    byte count

--- size_count/test.cc
+++ size_count/test.cc
@@ -4,13 +4,16 @@
 
 void test() {
   char msg[512];
-  std::string say_hi = "hello";
+  std::string say_hi = "helloworld";
   memset(msg, 0, sizeof(msg));
   memcpy(msg, say_hi.c_str(), say_hi.size());
   std::cout << say_hi.size() << "," << strlen(msg) << std::endl;
 
-  char val[] = "hello";
+  char val[] = "helloworld";
   std::cout << sizeof(val) << "," << strlen(val) << std::endl;
+
+  char* ptr = val;
+  std::cout << strlen(ptr) << std::endl;
 }
 
 int main() {
